f81e300c555abaf9acb0fbd500a51e30ea8e18da
OliverSkolePC <OIOl.skp@edu.mercantec.dk>
2021-09-09 12:21:58 +0200

    Few changes that can be used later, so that i can access basket from database

--- Views/Basket.php
+++ Views/Basket.php
@@ -1,3 +1,36 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "lagkagehuset";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM Users";
+$result = $conn->query($sql);
+
+
+
+// if ($result->num_rows > 0) {
+//     // output data of each row
+//     while($row = $result->fetch_assoc()) {
+//         echo "id: " . $row["ID"]. " - Name: " . $row["Name"]. " " . $row["Password"]. "<br>";
+//     }
+// } 
+// else {
+//     echo "0 results";
+// }
+  $conn->close();
+   
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
